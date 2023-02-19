d8b3b3dba0ca4ecb0f5f346318546a494354f4e0
Gasper Spagnolo <gasper.spagnolo@outlook.com>
2023-02-19 18:06:06 +0100

    Backend prototype finished

--- rust_backend/src/image.rs
+++ rust_backend/src/image.rs
@@ -8,9 +8,12 @@ use mongodb::{bson::doc, Client, Collection, options::FindOptions};
 use futures::stream::TryStreamExt;
 use serde::{Deserialize, Deserializer};
 use chrono::{DateTime, Utc};
+use futures::StreamExt;
+
 
 const DB_NAME: &str = "zyscan";
 const COLL_NAME: &str = "images";
+const MAJORITY_CLASS_PROBABILITY: f64 = 0.1;
 
 struct UtcDateTime(DateTime<Utc>);
 
@@ -46,7 +49,6 @@ impl<'de> Deserialize<'de> for UtcDateTime {
 #[get("/api/images")]
 async fn get_images(client: web::Data<Client>, params: web::Query<DateTimeQuery>) -> HttpResponse {
 
-    // convert params.dt to bson::DateTime
     let dt = mongodb::bson::DateTime::from_millis(params.dt.0.timestamp_millis());
   
     let collection: Collection<Image> = client.database(DB_NAME).collection(COLL_NAME);
@@ -55,11 +57,58 @@ async fn get_images(client: web::Data<Client>, params: web::Query<DateTimeQuery>
         .sort(doc! { "i_datetime": -1 })
         .build();
 
-    let stream = collection
+    let mut stream = collection
         .find(doc! { "i_datetime": { "$lt": dt } }, options)
         .await
         .expect("find should succeed");
      
+    if stream.next().await.is_none() {
+        return HttpResponse::BadRequest().json("No images found for the class!");
+    }
+
+    let mut images: Vec<Image> = Vec::new();
+    let mut stream = stream.into_stream();
+
+    while let Some(image) = stream.try_next().await.unwrap() {
+        images.push(image);
+    }
+
+    HttpResponse::Ok().json(images)    
+}
+
+#[derive(Deserialize)]
+struct ClassQuery {
+    class: String,
+    dt: UtcDateTime,
+}
+
+#[get("/api/images-by-class")]
+async fn get_images_by_class(client: web::Data<Client>, params: web::Query<ClassQuery>) -> HttpResponse {
+    
+    let dt = mongodb::bson::DateTime::from_millis(params.dt.0.timestamp_millis());
+
+    let collection: Collection<Image> = client.database(DB_NAME).collection(COLL_NAME);
+    let options = FindOptions::builder()
+        .limit(50)
+        .sort(doc! { "i_datetime": -1 })
+        .build();
+
+    let mut stream = collection.find(
+        doc! {
+            "classification_result.name": &params.class,
+            "$expr": {
+                "$gt": ["$classification_result.probability", MAJORITY_CLASS_PROBABILITY],
+                "i_datetime": { "$lt": dt }
+            }
+        },
+        options,
+        ).await
+        .expect("find should succeed");
+    
+    if stream.next().await.is_none() {
+        return HttpResponse::BadRequest().json("No images found for the class!");
+    }
+
     let mut images: Vec<Image> = Vec::new();
     let mut stream = stream.into_stream();
 
--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,62 +1,14 @@
 mod config;
-mod model;
 mod scan_im;
 mod image;
 
-use actix_rt::System;
-use actix_web::web::Json;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer};
-use model::User;
-use mongodb::{bson::doc, options::IndexOptions, Client, Collection, IndexModel};
-use std::thread;
 use scan_im::THUMBNAIL_LOCATION;
+
+use actix_rt::System;
+use actix_web::{web, App, HttpServer};
 use actix_files as fs;
-
-const DB_NAME: &str = "zyscan";
-const COLL_NAME: &str = "users";
-
-/// Adds a new user to the "users" collection in the database.
-#[post("/add_user")]
-async fn add_user(client: web::Data<Client>, user_req: Json<User>) -> HttpResponse {
-    let collection = client.database(DB_NAME).collection(COLL_NAME);
-    let result = collection.insert_one(user_req, None).await;
-    match result {
-        Ok(_) => HttpResponse::Ok().body("user added"),
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-    }
-}
-
-/// Gets the user with the supplied username.
-#[get("/get_user/{username}")]
-async fn get_user(client: web::Data<Client>, username: web::Path<String>) -> HttpResponse {
-    let username = username.into_inner();
-    let collection: Collection<User> = client.database(DB_NAME).collection(COLL_NAME);
-    match collection
-        .find_one(doc! { "username": &username }, None)
-        .await
-    {
-        Ok(Some(user)) => HttpResponse::Ok().json(user),
-        Ok(None) => {
-            HttpResponse::NotFound().body(format!("No user found with username {username}"))
-        }
-        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
-    }
-}
-
-/// Creates an index on the "username" field to force the values to be unique.
-async fn create_username_index(client: &Client) {
-    let options = IndexOptions::builder().unique(true).build();
-    let model = IndexModel::builder()
-        .keys(doc! { "username": 1 })
-        .options(options)
-        .build();
-    client
-        .database(DB_NAME)
-        .collection::<User>(COLL_NAME)
-        .create_index(model, None)
-        .await
-        .expect("creating an index should succeed");
-}
+use std::thread;
+use mongodb::Client;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -68,7 +20,6 @@ async fn main() -> std::io::Result<()> {
     let client = Client::with_uri_str(cfg.db_connection)
         .await
         .expect("failed to connect");
-    create_username_index(&client).await;
 
     // Scan exif data in a seperate thread
     thread::spawn(move || {
@@ -84,15 +35,14 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let mut app = App::new()
             .app_data(web::Data::new(client.clone()))
-            .service(add_user)
-            .service(get_user)
             .service(
                 fs::Files::new("/thumbnails", THUMBNAIL_LOCATION)
                     .show_files_listing()
                     .use_last_modified(true),
             )
            .service(image::get_classes)
-           .service(image::get_images);
+           .service(image::get_images)
+           .service(image::get_images_by_class);
         
         // Serve all the images from the config file
         for folder in &cfg.scan_folders {
