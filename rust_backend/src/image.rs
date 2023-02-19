7d5fa240d35640c8651bc427f1959fdc37ba1507
Gasper Spagnolo <gasper.spagnolo@outlook.com>
2023-02-19 16:13:25 +0100

    I will become datatime parsing pro

--- rust_backend/src/image.rs
+++ rust_backend/src/image.rs
@@ -6,13 +6,16 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use mongodb::{bson::doc, Client, Collection, options::FindOptions};
 use futures::stream::TryStreamExt;
+use serde::{Deserialize, Deserializer};
+use chrono::{DateTime, Utc};
 
 const DB_NAME: &str = "zyscan";
 const COLL_NAME: &str = "images";
 
+struct UtcDateTime(DateTime<Utc>);
+
 #[get("/api/iclasses")]
 async fn get_classes() -> HttpResponse {
-    // Open classes file and serve it
     let file = File::open(CLASSES_FILE).unwrap();
     let reader = BufReader::new(file);
     let mut classes = Vec::new();
@@ -22,26 +25,41 @@ async fn get_classes() -> HttpResponse {
     HttpResponse::Ok().json(classes) 
 }
 
-#[get("/api/images/{datetime}")]
-async fn get_images(client: web::Data<Client>, datetime: web::Path<String>) -> HttpResponse {
+#[derive(Deserialize)]
+struct DateTimeQuery {
+    dt: UtcDateTime,
+}
 
-    println!("get_images called");
- 
-    let _datetime = datetime.into_inner();
+impl<'de> Deserialize<'de> for UtcDateTime {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let dt = DateTime::parse_from_rfc3339(&s)
+            .map_err(serde::de::Error::custom)?
+            .with_timezone(&Utc);
+        Ok(UtcDateTime(dt))
+    }
+}
+
+#[get("/api/images")]
+async fn get_images(client: web::Data<Client>, params: web::Query<DateTimeQuery>) -> HttpResponse {
+
+    // convert params.dt to bson::DateTime
+    let dt = mongodb::bson::DateTime::from_millis(params.dt.0.timestamp_millis());
+  
     let collection: Collection<Image> = client.database(DB_NAME).collection(COLL_NAME);
     let options = FindOptions::builder()
         .limit(50)
-//        .sort(doc! { "i_datetime": -1 })
+        .sort(doc! { "i_datetime": -1 })
         .build();
 
-    //let stream = collection
-    //    .find(doc! { "i_datetime": { "$lt": &datetime } }, options)
-    //    .await
-    //    .expect("find should succeed");
-    //
-    let stream = collection.find(doc! {}, options).await.expect("find should succeed");
-
-    
+    let stream = collection
+        .find(doc! { "i_datetime": { "$lt": dt } }, options)
+        .await
+        .expect("find should succeed");
+     
     let mut images: Vec<Image> = Vec::new();
     let mut stream = stream.into_stream();
 
